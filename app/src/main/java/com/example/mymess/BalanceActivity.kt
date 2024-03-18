aca570049822cdab1e544a71cf2d689b33aeb857
Praveen <sspp.praveen2003@gmail.com>
2024-03-18 13:33:53 +0530

    refresh commit

--- app/src/main/java/com/example/mymess/BalanceActivity.kt
+++ app/src/main/java/com/example/mymess/BalanceActivity.kt
@@ -3,6 +3,8 @@ package com.example.mymess
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.mymess.Adapters.StudentAdapter
 import com.example.mymess.Models.StudentItemModel
 import com.example.mymess.databinding.ActivityBalanceBinding
@@ -10,67 +12,74 @@ import com.google.firebase.database.*
 
 class BalanceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBalanceBinding
-    private lateinit var mAdapter: StudentAdapter
-    private val stulist = mutableListOf<StudentItemModel>()
+    private lateinit var recyclerView:RecyclerView
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBalanceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val recyclerView = binding.balanceRecyclerview
+        swipeRefreshLayout = binding.swipeRefreshLayout
+        swipeRefreshLayout.setOnRefreshListener {
+            // Call a method to refresh data
+            recreate()
+        }
+
+        recyclerView = binding.balanceRecyclerview
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        mAdapter = StudentAdapter(stulist, true)
-        recyclerView.adapter = mAdapter
-
-        val balanceRef = FirebaseDatabase.getInstance().reference.child("balance")
-        val usersRef = FirebaseDatabase.getInstance().reference.child("users")
-
-        // Listen for changes in the balance node
-        balanceRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                stulist.clear() // Clear the list before adding updated data
-
-                // Iterate through each user in the balance node
-                dataSnapshot.children.forEach { userSnapshot ->
-                    val userId = userSnapshot.key
-
-                    // Fetch user details from the users node using the userId
-                    userId?.let { fetchUserDetails(it, usersRef) }
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // Handle onCancelled event
-            }
-        })
+        fetchDataFromDatabase()
     }
 
-    private fun fetchUserDetails(userId: String, usersRef: DatabaseReference) {
-        // Fetch user details (name and image URL) based on the user ID
-        usersRef.child(userId).addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(userSnapshot: DataSnapshot) {
-                if (userSnapshot.exists()) {
-                    val userid = userSnapshot.child("userid").getValue(String::class.java)
-                    val username = userSnapshot.child("name").getValue(String::class.java)
-                    val userImage = userSnapshot.child("profileImage").getValue(String::class.java)
+    private fun fetchDataFromDatabase(){
+        val balanceRef = FirebaseDatabase.getInstance().reference.child("balance")
+        balanceRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val stulist = mutableListOf<StudentItemModel>()
+                var userCount: Long = 0 // Counter to track number of ValueEventListener callbacks
 
-                    val stuitem = username?.let {
-                        if (userImage != null && userid != null) {
-                            StudentItemModel(userid, it, "", userImage)
-                        } else {
-                            null // Return null if either userImage or userid is null
+                for (userSnapshot in dataSnapshot.children) {
+                    val userId = userSnapshot.key
+
+                    // Fetch user details (name and image URL) based on the user ID
+                    val userRef = FirebaseDatabase.getInstance().reference.child("users").child(userId!!)
+                    userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(userSnapshot: DataSnapshot) {
+                            userCount++
+                            if (userSnapshot.exists()) {
+                                val userid = userSnapshot.child("userid").getValue(String::class.java)
+                                val username = userSnapshot.child("name").getValue(String::class.java)
+                                val userImage = userSnapshot.child("profileImage").getValue(String::class.java)
+
+                                val stuitem = username?.let {
+                                    if (userImage != null && userid != null) {
+                                        StudentItemModel(userid, it, "", userImage)
+                                    } else {
+                                        null // Return null if either userImage or userid is null
+                                    }
+                                }
+
+                                stuitem?.let {
+                                    stulist.add(it) // Add to the list only if stuitem is not null
+                                }
+
+                                // If all callbacks have completed, update the adapter with the data
+                                if (userCount == dataSnapshot.childrenCount) {
+                                    val mAdapter = StudentAdapter(stulist, true)
+                                    recyclerView.adapter = mAdapter
+                                    mAdapter.update(stulist) // Call update() function here
+                                }
+                            }
                         }
-                    }
 
-                    stuitem?.let {
-                        stulist.add(it) // Add to the list only if stuitem is not null
-                    }
-
-                    // Notify the adapter of changes after fetching user details
-                    mAdapter.notifyDataSetChanged()
+                        override fun onCancelled(error: DatabaseError) {
+                            userCount++ // Increment counter even if onCancelled occurs
+                            // Handle onCancelled event
+                        }
+                    })
                 }
+                swipeRefreshLayout.isRefreshing = false
             }
 
             override fun onCancelled(error: DatabaseError) {
